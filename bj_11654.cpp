054aa5fc164c41042caf8d7b01010f7b894f2acb
dahyun1018 <dhyoo2k@gmail.com>
2020-04-04 15:19:04 +0900

    아스키코드값 출력

---
+++ bj_11654.cpp
@@ -0,0 +1,13 @@
+//BOJ 11654 아스키코드
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	char a;				//아스키 코드는 문자를 숫자로 표현하는 코드
+	cin >> a;			//문자를 입력받아서
+
+	cout << (int) a;	//숫자로 표현
+
+	return 0;
+}
